149117f0b6ae6d7a671b7de1847e23429906fb79
akari <neilaaa.osmani@gmail.com>
2025-05-09 18:08:52 +0200

    travail animator

--- Assets/script/MouvementSimple.cs
+++ Assets/script/MouvementSimple.cs
@@ -1,46 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class MouvementSimple : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float vitesseDeplacement = 5f;
+    [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
-    private Vector2 mouvement;
+    private Vector2 moveInput;
+    private Animator animator;
 
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-
-        mouvement = Vector2.zero;
-
-
-        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow))
-        {
-            mouvement.y = 1f;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            mouvement.y = -1f;
-        }
-        else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            mouvement.x = -1f;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            mouvement.x = 1f;
-        }
-
-
-        if (mouvement.x != 0) mouvement.y = 0;
-        if (mouvement.y != 0) mouvement.x = 0;
+        // Mettre à jour l'animation même quand le joueur ne bouge pas
+        UpdateAnimation();
     }
-     void FixedUpdate()
-     {
 
-    rb.linearVelocity = mouvement.normalized * vitesseDeplacement;
-     }
+    // FixedUpdate est mieux pour la physique
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void UpdateAnimation()
+    {
+        bool isMoving = moveInput.magnitude > 0.1f;
+        animator.SetBool("isWalking", isMoving);
+
+        if (isMoving)
+        {
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+
+            // Enregistrer la dernière direction seulement quand on bouge
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+    }
 }
